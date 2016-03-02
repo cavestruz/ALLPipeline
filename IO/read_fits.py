731e467f2b5660e8dcd3bdd40b53f65cf3b0da2e
Matthew Lightman <matthew.lightman@chase.com>
2016-03-01 22:34:43 -0600

    Add fits to png converter.

--- IO/read_fits.py
+++ IO/read_fits.py
@@ -1,13 +1,19 @@
 import astropy.io.fits as pyfits
 import pylab as pl
+from scipy.misc import imsave
 
 def get_fits_obj(fitsfile) :
     '''Returns contents of fitsfile in numpy array format'''
     return pyfits.getdata(fitsfile)
 
-def visualize_fits(fits_io,figname) :
+def visualize_fits(fitsfile, figname, colorbar = False) :
     '''Saves fitsfile contents as an image, <figname>.png'''
     pl.figure()
-    pl.contour(fits_io)
-    pl.colorbar()
+    pl.contour(get_fits_obj(fitsfile))
+    if colorbar:
+        pl.colorbar()
     pl.savefig(figname+'.png')
+
+def fits_to_png(fitsfile, png_out):
+    img_arr = pyfits.getdata(fitsfile)
+    imsave(png_out, img_arr)
