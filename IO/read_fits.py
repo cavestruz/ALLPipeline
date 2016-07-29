ade9d547d9dc30cabb98cad0fa8903caaa826437
Camille Avestruz <briarrose5187@gmail.com>
2016-07-29 12:09:25 -0400

    Added a name == main to read_fits.py so one can simply have a glob of command line arguments to go from fits to png.

--- IO/read_fits.py
+++ IO/read_fits.py
@@ -1,6 +1,7 @@
 import astropy.io.fits as pyfits
 import pylab as pl
 from scipy.misc import imsave
+import sys, glob
 
 def get_fits_obj(fitsfile) :
     '''Returns contents of fitsfile in numpy array format'''
@@ -17,3 +18,8 @@ def visualize_fits(fitsfile, figname, colorbar = False) :
 def fits_to_png(fitsfile, png_out):
     img_arr = pyfits.getdata(fitsfile)
     imsave(png_out, img_arr)
+
+if __name__ == "__main__" : 
+    for fits in glob.glob(sys.argv[1]) :
+        pngname = fits[:-4]
+        fits_to_png(fits, pngname+'.png')
