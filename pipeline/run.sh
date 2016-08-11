d7eaa8e4b8a9b4a589377025b5b9de0b7dc7d11e
BabyOstrich <Camille Avestruz>
2016-08-11 12:16:26 -0500

    No longer tracking run.sh

--- pipeline/run.sh
+++
@@ -1,2 +0,0 @@
-python run_pipeline.py "/Users/matt/Camille/Repos/code/StrongCNN/data/realistic_HST_data/*_unlensed_*.fits" "/Users/matt/Camille/Repos/code/StrongCNN/data/realistic_HST_data/*_lensed_*.fits"
- 
