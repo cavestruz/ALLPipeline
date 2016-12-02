14741062141131fc0893177a175cf3ace547e8b4
Camille Avestruz <avestruz@uchicago.edu>
2016-12-01 22:36:31 -0600

    Script to autoplot these high/low/etc. scoring files added

---
+++ models/plot_high_border_low_scoring_filenames.py
@@ -0,0 +1,19 @@
+import StrongCNN.utils.read_model_out as rmo
+import StrongCNN.plot_tools.tile_fits as tile_fits
+
+import sys
+
+modeldir = sys.argv[1]
+
+image_types = [ 'true_positives',
+                'true_negatives',
+                'false_positives',
+                'false_negatives',
+                'borderline_positives',
+                'borderline_negatives',
+                ]
+
+for itype in image_types :
+    idata = rmo.read_tpr_filenames(modeldir+'/'itype+'.txt')
+    tile_fits.plot_set(idata['fits_file'], modeldir+'/'+itype+'.pdf')
+    
