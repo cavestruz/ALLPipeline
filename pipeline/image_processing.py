06ff05d926134dac9c40ffda149855d9e2134199
BabyOstrich <Camille Avestruz>
2017-01-23 08:26:00 -0600

    Added a positive definite log and normalized image transform.  Still need to test with old data, then can run on challenge

--- pipeline/image_processing.py
+++ pipeline/image_processing.py
@@ -43,6 +43,53 @@ class MedianSmooth(BaseEstimator):
     def fit_transform(self, images, y = None):
         return self.transform(images)
 
+class Clip(BaseEstimator) :
+    '''Numpy clip'''
+
+    def __init__( self, clip_min=1e-6, clip_max=1e100):
+
+        self.clip_min = clip_min
+        self.clip_max = clip_max
+
+    def fit(self, image, y=None) : 
+
+        return self
+
+    def transform( self, images ) :
+
+        return np.array( [ np.clip( image, self.clip_min, self.clip_max ) 
+                           for image in images ] )
+
+    def fit_transform( self, images, y = None ) :
+
+        return self.transform(images)
+
+class LogPositiveDefinite(BaseEstimator) :
+    ''' Shift all values to positive definite with options for taking
+    log of image and making log positive definite. Return normalized
+    values.'''
+
+    def __init__( self, log = True ) : 
+        self.log = log
+
+    def fit( self, images, y = None ) :
+        return self
+
+    def _make_positive( self, image ) :
+        ''' Ensure that the minimum value is just above zero. '''
+        return image - image.min() + np.abs(image.min())
+
+    def _normalize( self, image ) :
+ 
+        if self.log : 
+            return np.log( self._make_positive(image) ) / np.log( self._make_positive(image) ).max()
+        else :
+            return self._make_positive(image) / self._make_positive(image).max()
+
+    def transform( self, images ) :
+        return np.array( [ self._normalize(image) for image in images ] )
+
+
 class HOG(BaseEstimator):
     def __init__(self, orientations = 9, pixels_per_cell = (8, 8),
                  cells_per_block = (3, 3) ):
