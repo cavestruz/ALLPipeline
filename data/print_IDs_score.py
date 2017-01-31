a45b49d9a59f2dc0668185f900d095209b829a13
BabyOstrich <Camille Avestruz>
2017-01-31 17:15:37 -0600

    Now have a script to take in the tpr filenames textfile and create IDs and score, ordered by ID

--- data/print_IDs_score.py
+++ data/print_IDs_score.py
@@ -1,1 +1,13 @@
+import sys, os
+from glob import glob
 import pandas as pd
+
+def print_ID_and_score( tprfile='tpr_filenames.txt', outfile='submission.txt' ) :
+    df = pd.read_csv( tprfile, sep=' ') 
+    # http://chrisalbon.com/python/pandas_regex_to_create_columns.html
+    df['IDs'] = df['filename'].str.extract('(\d\d\d\d\d\d)')
+    df.sort('IDs',inplace=True) 
+    df.to_csv( path_or_buf=outfile, sep=' ', columns=['IDs', 'score'], index=False )
+
+    
+print_ID_and_score( tprfile='challenge_data/SpaceBased/tpr_filenames.txt' )
