f48c745242faaa27f8d3bd5e68eb13bb23b505e1
BabyOstrich <Camille Avestruz>
2017-01-31 21:51:00 -0600

    More informative filename

--- data/print_IDs_score.py
+++ data/print_IDs_score.py
@@ -2,7 +2,7 @@ import sys, os
 from glob import glob
 import pandas as pd
 
-def print_ID_and_score( tprfile='tpr_filenames.txt', outfile1='avestruz_submission_by_id.txt', outfile2='avestruz_submission_by_score.txt' ) :
+def print_ID_and_score( tprfile='tpr_filenames.txt', outfile1='avestruz_space_submission_ordered_by_id.txt', outfile2='avestruz_space_submission_ordered_by_score.txt' ) :
     df = pd.read_csv( tprfile, sep=' ') 
     # http://chrisalbon.com/python/pandas_regex_to_create_columns.html
     df['IDs'] = df['filename'].str.extract('(\d\d\d\d\d\d)')
