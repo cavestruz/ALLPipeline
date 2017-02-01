dbc5f8a07a1d69d78e7fb5589f37e20b2f18414f
BabyOstrich <Camille Avestruz>
2017-01-31 21:49:47 -0600

    Now prints out submission id+score, ordered by both id and by score

--- data/print_IDs_score.py
+++ data/print_IDs_score.py
@@ -2,12 +2,13 @@ import sys, os
 from glob import glob
 import pandas as pd
 
-def print_ID_and_score( tprfile='tpr_filenames.txt', outfile='submission.txt' ) :
+def print_ID_and_score( tprfile='tpr_filenames.txt', outfile1='avestruz_submission_by_id.txt', outfile2='avestruz_submission_by_score.txt' ) :
     df = pd.read_csv( tprfile, sep=' ') 
     # http://chrisalbon.com/python/pandas_regex_to_create_columns.html
     df['IDs'] = df['filename'].str.extract('(\d\d\d\d\d\d)')
+    df.to_csv( path_or_buf=outfile2, sep=' ', columns=['IDs', 'score'], index=False )
     df.sort('IDs',inplace=True) 
-    df.to_csv( path_or_buf=outfile, sep=' ', columns=['IDs', 'score'], index=False )
+    df.to_csv( path_or_buf=outfile1, sep=' ', columns=['IDs', 'score'], index=False )
 
     
 print_ID_and_score( tprfile='challenge_data/SpaceBased/tpr_filenames.txt' )
