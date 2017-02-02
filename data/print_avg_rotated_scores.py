e1def11fd50b3887a4767527e4ea506b12b341a3
Camille Avestruz <avestruz@uchicago.edu>
2017-02-01 22:20:40 -0600

    printing a debug for id, label, avg scores, all scores for each rotation

--- data/print_avg_rotated_scores.py
+++ data/print_avg_rotated_scores.py
@@ -6,7 +6,11 @@ from StrongCNN.utils.model_info import generate_AUC_fpr_tpr
 scoresdir = sys.argv[1] 
 # sub_type = sys.argv[2]
 
-def print_ID_and_avg_score( fdir='.', outfile='/avg_rotated_scores.txt', AUC_file='/rotated_aucs.txt' ) :
+def print_ID_and_avg_score( fdir='.', 
+                            outfile1='/avestruz_submission_avg_scores_ordered_by_id.txt', 
+                            outfile2='/avestruz_submission_avg_scores_ordered_by_score.txt', 
+                            outfile3='/print_all_scores_for_debugging.txt',
+                            AUC_file='/rotated_aucs.txt' ) :
     '''Create four dataframes, order by id, create new dataframe and print'''
 
     df = {}
@@ -17,13 +21,15 @@ def print_ID_and_avg_score( fdir='.', outfile='/avg_rotated_scores.txt', AUC_fil
         print df[rotation]
         df[rotation]['IDs'] = df[rotation]['filename'].str.extract('(\d\d\d\d\d\d)')
         df[rotation].sort_values(by='IDs',inplace=True) 
-
+    
     # append other scores to first
     df[rotated_scores[0]]['avgscores'] = df[rotated_scores[0]]['score']/4.
     for rotation in rotated_scores[1:] :
         df[rotated_scores[0]]['avgscores'] += df[rotation]['score']/4.
+        df[rotated_scores[0]][rotation] = df[rotation]['score']
 
-    df[rotated_scores[0]].to_csv( path_or_buf=fdir+outfile, sep=' ', columns=['IDs', 'score'], index=False )
+    df[rotated_scores[0]].to_csv( path_or_buf=fdir+outfile1, sep=' ', columns=['IDs', 'avgscores'], index=False )
+    df[rotated_scores[0]].to_csv( path_or_buf=fdir+outfile3, sep=' ', columns=['IDs', 'label', 'avgscores', 'score']+rotated_scores[1:], index=False )
 
     if AUC_file != None :
         f = open(fdir+AUC_file, 'w')
@@ -33,6 +39,8 @@ def print_ID_and_avg_score( fdir='.', outfile='/avg_rotated_scores.txt', AUC_fil
             AUC, _ = generate_AUC_fpr_tpr( df[rotation]['label'], df[rotation]['score'] )
             f.write('%1.6f\n' % AUC )
 
+    df[rotated_scores[0]].sort_values(by='avgscores',inplace=True) 
+    df[rotated_scores[0]].to_csv( path_or_buf=fdir+outfile2, sep=' ', columns=['IDs', 'scores'], index=False )
 
 print_ID_and_avg_score( fdir=scoresdir,
                         )
