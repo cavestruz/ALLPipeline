01ddfb58142afd320931abd1abbc038f88d81337
BabyOstrich <Camille Avestruz>
2017-02-03 00:35:00 -0600

    adjusting naming

--- data/combine_bands_parallel.py
+++ data/combine_bands_parallel.py
@@ -14,11 +14,12 @@ time_now = time.time()
 # combined_dir = '/data/avestruz/StrongCNN/Challenge/FinalData/GroundBased/combined_bands'
 
 
-IDs_glob=['*100?[12]*', '*100?[34]*', '*100?[56]', '*100?[78]*', '*100?[90]*' ]
-band_dir = ['/data/avestruz/StrongCNN/Challenge/GroundBased/GroundBasedTraining/lensed-Band'+str(i)+'/' for i in range(1,5) ]
-IDs_dir=band_dir[0]
-combined_dir = '/data/avestruz/StrongCNN/Challenge/GroundBased/GroundBasedTraining/sample_combined_dir/'
+# IDs_glob=['*100?[12]*', '*100?[34]*', '*100?[56]', '*100?[78]*', '*100?[90]*' ]
+# band_dir = ['~/Downloads/Data_KiDS_Big.*/Public/Band'+str(i)+'/' for i in range(1,5) ]
+# IDs_dir=band_dir[0]
 
+# IDs_glob=['/home/babyostrich/Downloads/Data_KiDS_Big.'+str(i)+'/Public/Band1/' for i in range(0,11)]
+combined_dir = '/home/babyostrich/Documents/Repos/StrongCNN/data/FinalChallengeData/GroundBasedData/combined_data'
 
           
 def get_file_by_ID( directory, ID ) :
@@ -41,17 +42,20 @@ def get_IDs( IDs_directory, IDs_glob='*' ) :
                  for filename in filenames ]
 
 def write_combined_fits_for_IDs( IDs_glob, IDs_directory=IDs_dir, band_directories=band_dir, combined_directory=combined_dir ) :
+    band_directories = [ '/home/babyostrich/Downloads/Data_KiDS_Big.'+str(i)+'/Public/Band'+str(i)+'/' for i in range(1,5) ]
+    IDs_directory = band_directories[0]
+
     if not os.path.exists(combined_directory) :
         print "making ", combined_directory
         os.mkdir( combined_directory ) 
-    for ID in get_IDs( IDs_directory, IDs_glob=IDs_glob ) :
+    for ID in get_IDs( IDs_directory ) :
         write_combined_fits(  combined_directory, band_directories, ID  )
 
 
 if __name__ == "__main__" :
     p = Pool(5) 
 
-    p.map(write_combined_fits_for_IDs, IDs_glob)
+    p.map(write_combined_fits_for_IDs, range(0,11))
 
     print "Time taken for ", [IDs_dir]+IDs_glob, time.time() - time_now
 
