337a8df1d290d933c25fcc272fc8cbe96ca3dc2e
Camille Avestruz <avestruz@uchicago.edu>
2017-02-02 20:12:01 -0600

    now have different bash scripts to get scoring on train, test, and both one right after the other.  Note, if train has many more files than test, it will clearly take much longer to score than test.  Suggest breaking the scoring into pieces.

--- models/print_filenames_scores_of_trainset.sh
+++ models/print_filenames_scores_of_trainset.sh
@@ -1,6 +1,5 @@
 dir=$1
 
 python test_trained_model.py -s $dir/filenames_scores.txt $dir train >> $dir/model_train.out
-python test_trained_model.py -s $dir/filenames_scores.txt $dir test >> $dir/model_test.out
 python ../data/print_avg_rotated_scores.py $dir train
-python ../data/print_avg_rotated_scores.py $dir test
+
