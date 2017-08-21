b47291e2f8bf0e699929433baa28a0e6553ac4d1
Hanjue Zhu <hanjuezhu@orion.uchicago.edu>
2017-08-21 17:02:21 -0500

    Added a setup.py file so we can create our first release.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ALLPipeline',
+    version='0.1dev',
+    packages=['allpipeline',],
+    license='The MIT License',
+    long_description=open('README.md').read(),
+)
