48bcf723725cc31654f97a0f744d0cef734dd76c
Camille Avestruz <avestruz@uchicago.edu>
2017-08-21 17:08:16 -0500

    added description and author info in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from distutils.core import setup
 setup(
     name='ALLPipeline',
     version='0.1dev',
+    description='Machine Learning Pipeline',
+    author='Camille Avestruz, Matthew Lightman, Hanjue Zhu',
+    author_email='camille.avestruz@yale.edu',
     packages=['allpipeline',],
     license='The MIT License',
     long_description=open('README.md').read(),
